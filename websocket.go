bbacfa55408bd055d9d62cd9426cd49602490242
stripe-island <109348950+stripe-island@users.noreply.github.com>
2022-08-30 14:05:46 +0900

    :recycle: Webブラウザから利用できるようにクロスオリジン対応

--- websocket.go
+++ websocket.go
@@ -7,7 +7,12 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{} // use default options
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
 var clients = make(map[*websocket.Conn]bool)
 var broadcast = make(chan Room)
 
